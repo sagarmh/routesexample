3853d7d2cd4aec758aae4a4f2d74bb4e73510a34
sagmhatr1 <sagar.mhatre@publicissapient.com>
2023-05-30 20:10:50 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Courses from "./pages/Courses";
+import Live from "./pages/Live";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <nav>
+        <ul>
+          <Link to="/" className="list">
+            Home
+          </Link>
+          <Link to="/course" className="list">
+            Courses
+          </Link>
+          <Link to="/live" className="list">
+            Live course
+          </Link>
+          <Link to="/contact" className="list">
+            Contact
+          </Link>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course" element={<Courses />} />
+        <Route path="/live" element={<Live />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
